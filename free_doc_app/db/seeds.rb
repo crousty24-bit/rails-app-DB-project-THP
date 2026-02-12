c88c0a546fbcfe411fdde68971b34d0d4f6e4a38
crousty24-bit <allen.kch24@gmail.com>
2026-02-12 17:59:05 +0100

    add model City relations w/ tables
    update models doctor, patient, appointment to belongs_to City
    
    update seeds: clean DB & generate new lists w/ 10 new cities sample

--- free_doc_app/db/seeds.rb
+++ free_doc_app/db/seeds.rb
@@ -4,8 +4,18 @@ puts "Cleaning DB ..."
 Appointment.destroy_all #destroy children bedore parents to avoid conflicts
 Doctor.destroy_all
 Patient.destroy_all
+City.destroy_all
 
 specialties = ["Dentist", "Surgery", "Psychiatry", "Optometry", "Ophtalmology", "Radiology", "Plastic Surgery", "Orthopedics", "Rhinology", "Hematology", "Dermatology"]
+
+puts "------------------------"
+puts "Creating Cities..."
+cities_list = []
+10.times do
+  city = City.create(name: Faker::Address.city)
+  cities_list << city
+end
+puts "Cities created: #{City.count}"
 
 puts "------------------------"
 puts "Creating 100 doctors ..."
@@ -15,7 +25,8 @@ doctors_list = []
     first_name: Faker::Name.first_name, 
     last_name: Faker::Name.last_name, 
     specialty: specialties.sample,
-    zip_code: Faker::Address.zip_code
+    zip_code: Faker::Address.zip_code,
+    city: cities_list.sample
   )
   doctors_list << doctor #save in array for Appointment Model
 end
@@ -25,7 +36,8 @@ patients_list = []
 500.times do
   patient = Patient.create(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    last_name: Faker::Name.last_name,
+    city: cities_list.sample
   )
   patients_list << patient #save in array for Appointment Model
 end
@@ -35,7 +47,8 @@ puts "Creating 1000 appointments ..."
   Appointment.create(
     date: Faker::Time.forward(days: 90, period: :morning, format: :long), #set random date in within max90 days
     doctor: doctors_list.sample, #arrays sample to link doctor/patient in appointment
-    patient: patients_list.sample
+    patient: patients_list.sample,
+    city: cities_list.sample
   )
 end
 
