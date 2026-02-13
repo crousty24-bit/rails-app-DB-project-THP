10aa2b38a5fef92e07040ce61e836e4e3b127eec
crousty24-bit <allen.kch24@gmail.com>
2026-02-13 10:52:51 +0100

    feat: update seeds to fix N-N relation for doctors/specialities, now doctors can have between 1 to 3 randoms specialties
    add gem colorize
    
    add colorize to seeds to improve visuals & clarity

--- free_doc_app/db/seeds.rb
+++ free_doc_app/db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
+require 'colorize'
 
-puts "Cleaning DB ..."
+puts "Cleaning DB ...".colorize(:blue)
 Appointment.destroy_all #destroy children(join tables) before parents to avoid conflicts
 JoinTableDoctorSpecialty.destroy_all
 Doctor.destroy_all
@@ -9,26 +10,26 @@ Specialty.destroy_all
 City.destroy_all
 
 puts "------------------------"
-puts "Creating Cities..."
+puts "Creating Cities...".colorize(:light_blue)
 cities_list = []
 10.times do
   city = City.create(name: Faker::Address.city)
   cities_list << city
 end
-puts "Cities created: #{City.count}"
+puts "Cities created: #{City.count}".colorize(:light_green)
 
 puts "------------------------"
-puts "Creating Specialties..."
+puts "Creating Specialties...".colorize(:light_blue)
 specialties_list = ["Dentist", "Surgery", "Psychiatry", "Optometry", "Ophtalmology", "Radiology", "Plastic Surgery", "Orthopedics", "Rhinology", "Hematology", "Dermatology"]
 specialties = []
 specialties_list.each do |spec_name|
   specialty = Specialty.create(name: spec_name)
   specialties << specialty
 end
-puts "Specialties created : #{Specialty.count}"
+puts "Specialties created : #{Specialty.count}".colorize(:light_green)
 
 puts "------------------------"
-puts "Creating 100 doctors ..."
+puts "Creating 100 doctors ...".colorize(:light_blue)
 doctors_list = []
 100.times do
   doctor = Doctor.create(
@@ -38,15 +39,20 @@ doctors_list = []
     city: cities_list.sample
   )
   doctors_list << doctor #save in array for Appointment Model
-  JoinTableDoctorSpecialty.create( #assign specialty from join table
-    doctor: doctor, 
-    specialty: specialties.sample
-  )
+
+  number_of_specs = rand(1..3) #assign 1 to 3 random specs from array sample
+  random_specs = specialties.sample(number_of_specs)
+  random_specs.each do |spec|
+    JoinTableDoctorSpecialty.create( #assign specialty from join table
+      doctor: doctor, 
+      specialty: spec
+    )
+  end
 end
-puts "Doctors created : #{Doctor.count}"
+puts "Doctors created : #{Doctor.count}".colorize(:light_green)
 
 puts "------------------------"
-puts "Creating 500 patients ..."
+puts "Creating 500 patients ...".colorize(:light_blue)
 patients_list = []
 500.times do
   patient = Patient.create(
@@ -58,10 +64,10 @@ patients_list = []
   )
   patients_list << patient #save in array for Appointment Model
 end
-puts "Patients created : #{Patient.count}"
+puts "Patients created : #{Patient.count}".colorize(:light_green)
 
 puts "------------------------"
-puts "Creating 1000 appointments ..."
+puts "Creating 1000 appointments ...".colorize(:light_blue)
 1000.times do
   Appointment.create(
     date: Faker::Time.forward(days: 90, period: :morning, format: :long), #set random date in within max90 days
@@ -70,6 +76,6 @@ puts "Creating 1000 appointments ..."
     city: cities_list.sample
   )
 end
-puts "Appoinments created: #{Appointment.count}"
-
-puts "--- Seeds done ! ---"
+puts "Appoinments created: #{Appointment.count}".colorize(:light_green)
+puts "------------------------"
+puts "--- Seeds done ! ---".colorize(:light_green)
